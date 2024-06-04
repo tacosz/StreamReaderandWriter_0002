0626609b0564dd7d80ef25ecb7c865bba78a7f25
tacosz <nabilnasruddin.offc@gmail.com>
2024-06-04 18:27:38 +0700

    fungsi try di dalamnya terdapat 3 array dan menampilkan array yg di panggil

---
+++ exceptionStandard/exceptionStandard.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <exception>
+#include <array>
+using namespace std;
+
+int main()
+{
+    cout << "Awal program" << endl;
+    try
+    {
+        array <int, 3> data = { 6, 7, 8 };
+        cout << data.at(2) << endl;
+    }
+}
