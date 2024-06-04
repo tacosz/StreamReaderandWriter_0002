5d9286200c67aebe6c4bc3e0a5787cd9afb277c4
tacosz <nabilnasruddin.offc@gmail.com>
2024-06-04 17:51:18 +0700

    mengubah infile.open

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -32,7 +32,7 @@ int main()
 	//membuka file dalam mode membaca
 	ifstream infile;
 	//menunjuk ke sebuah file
-	infile.open("contohfile.txt");
+	infile.open(NamaFile + ".txt", ios::in);
 
 	cout << endl << ">= Membuka dan membaca file " << endl;
 	//jika file ada maka
